907cbf9ae46a8b2eae4b2c2448079528d2b980c0
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2022-09-21 22:19:05 +0200

    All models and Relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'body' => 'array'
+    ];
+
+    public function getTitleUpperCaseAttribute()
+    {
+        return strtoupper($this->title);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = strtolower($value);
+    }
+
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'post_id');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'post_user', 'post_id', 'user_id');
+    }
 }
