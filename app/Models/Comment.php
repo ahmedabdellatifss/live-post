068d3565d8b5e1b02e8d5fc2aded32ef3f6f61f4
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2022-10-20 21:16:53 +0200

    Unit Testing Essential Tips

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,6 +9,12 @@ class Comment extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'body',
+        'user_id',
+        'post_id',
+
+    ];
     protected $casts = [
         'body' => 'array'
     ];
