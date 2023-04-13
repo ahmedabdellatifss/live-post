1a48feaef36b0c7a6e8f6d122c1b202f05a7a27b
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2023-04-14 01:28:18 +0200

    fortify_Email Verification

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Mail\VerfiyMail;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Mail\VerifyMail;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,5 +29,14 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+
+        VerifyEmail::toMailUsing(function($notifiable, $url){
+            return new VerfiyMail($notifiable, $url);
+        });
+
+        // VerifyEmail::createUrlUsing(function(){
+        //     // logic here to create your own custom Verfication Url
+        //     // I prefer to use what laravel has already provided us
+        // });
     }
 }
---
+++ resources/views/mail/verify.blade.php
@@ -0,0 +1,7 @@
+@component('mail::message')
+
+# OIIII
+
+## Click the link here [here]({{$url}})
+
+@endcomponent
