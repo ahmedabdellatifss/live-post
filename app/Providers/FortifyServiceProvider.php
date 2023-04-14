d9511138848f9e5c285b60f37941627b9ddc79d4
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2023-04-14 02:04:24 +0200

    fortify_Customising Authentication logic

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -3,13 +3,17 @@
 namespace App\Providers;
 
 use App\Actions\Fortify\CreateNewUser;
+use App\Models\User;
+use App\Actions\Fortify\DummyDummy;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Features;
 use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
@@ -40,6 +44,31 @@ class FortifyServiceProvider extends ServiceProvider
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+
+        // Customis the authentication logic
+        // Fortify::authenticateUsing(function(Request $request){
+        //     $user = User::find(1);
+        //     return $user;
+        //     // grab credentials from request
+
+        //     // lookup user from db
+
+        //     // verify credentials
+
+        //     //return user model if correct
+        // });
+
+        //if you need to handl logic if the user login from the diffrent device or different unusual location and you want to tell him
+        Fortify::authenticateThrough(function (Request $request){
+            return array_filter([
+                config('fortify.limiters.login') ? null : EnsureLoginIsNotThrottled::class,
+                Features::enabled(Features::twoFactorAuthentication()) ? RedirectIfTwoFactorAuthenticatable::class : null,
+               DummyDummy::class,
+               DummyDummy::class,
+                AttemptToAuthenticate::class,
+                PrepareAuthenticatedSession::class,
+            ]);
         });
     }
 }
